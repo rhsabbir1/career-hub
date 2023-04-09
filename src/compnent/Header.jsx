4b1cf432fef0b753e73111d4a28db79bd1a1383d
rhsabbir1 <rhsabbir090@gmai.com>
2023-04-09 21:57:07 +0600

    install npm pakage & react touter dom & tailwind css

---
+++ src/compnent/Header.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            {/* header  */}
+            <div className='flex justify-around items-center py-4 bg-purple-100 '>
+            <div className='text-3xl font-bold'>IT Job Hunt</div>
+            <div className=''>
+                <a className='mr-4 text-2xl' href="">Home</a>
+                <a className='mr-4 text-2xl' href="">Blog</a>
+                <a className='mr-4 text-2xl' href="">Star Applying</a>
+            </div>
+            <div>
+                <button className='font-bold text-white bg-purple-400 py-2 px-4 rounded-md'>Star Applying</button>
+            </div>
+        </div>
+        </div>
+    );
+};
+
+export default Header;
