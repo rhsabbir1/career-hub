e76ee65629be8ecf0045c53bec9350d6256d4d86
rhsabbir1 <rhsabbir090@gmai.com>
2023-04-12 16:41:38 +0600

    erreo solvd

--- src/compnent/JobLIst/JobCatagory.jsx
+++ src/compnent/JobLIst/JobCatagory.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const JobCatagory = ({jobCatagory}) => {
     return (
         <div className='h-52 bg-slate-200 p-5 rounded-md '>
-            <img className='w-2/5 h-22 rounded-md' src={jobCatagory.picture} alt="" />
+            <img className='md:w-2/5 w-40 h-16 md:h-22 rounded-md' src={jobCatagory.picture} alt="" />
             <div className='mt-10'>
                 <h2 className='text-xl'>{jobCatagory.name}</h2>
                 <p>{jobCatagory.vacancy} + Jobs Available</p>
